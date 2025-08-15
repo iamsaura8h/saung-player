a810383b07b7f3930934d445bb65f3eae1b48930
iamsaura8h <saurabhchaudhary.net@gmail.com>
2025-08-16 03:33:11 +0530

    added audio player to play songs

--- ui/script.js
+++ ui/script.js
@@ -1,6 +1,7 @@
 // ui/script.js
 const selectFileBtn = document.getElementById("selectFile");
 const titleElement = document.querySelector("h2");
+const audioPlayer = document.getElementById('audioPlayer');
 
 selectFileBtn.onclick = async () => {
     const file = await window.electronAPI.selectFile();
@@ -8,8 +9,19 @@ selectFileBtn.onclick = async () => {
     if (!file.canceled && file.filePaths.length > 0) {
         const fullPath = file.filePaths[0];
         const fileName = fullPath.split("\\").pop(); // Extract filename only
-        titleElement.textContent = fileName; // Update <h2> with song name
+
+         // Update <h2> with song name
+        titleElement.textContent = fileName;
+
+        // Load and play the song
+        audioPlayer.src = `file:///${fullPath.replace(/\\/g, "/")}`;
+        audioPlayer.style.display="block";
+        audioPlayer.play().catch(error => console.log("Playback Failed:",err));
+
     } else {
         titleElement.textContent = "No file selected";
-    }
+        audioPlayer.style.display = "none";
+        audioPlayer.pause();
+    }   
+
 };
